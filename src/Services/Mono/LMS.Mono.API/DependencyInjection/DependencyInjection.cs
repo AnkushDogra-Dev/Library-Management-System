f9d709984f816bc61742f5bcc0f1b6a34b7cfabb
ditsd <ditsd@DITSDEV242>
2025-01-25 18:14:18 +0530

    test

--- src/Services/Mono/LMS.Mono.API/DependencyInjection/DependencyInjection.cs
+++ src/Services/Mono/LMS.Mono.API/DependencyInjection/DependencyInjection.cs
@@ -1,11 +1,12 @@
 using LMS.BooksRecordService.API.DependencyInjection;
 using LMS.BorrowersRecord.API.DependencyInjection;
+using LMS.Identity.API.DependencyInjection;
 
 namespace src.Services.Mono.DependencyInjection {
 	public static class DependencyInjection {
 		public static IServiceCollection AddMonoApi(this IServiceCollection services, IConfiguration configuration)
 		{
-			// services.AddIdentityService(configuration);
+			services.AddIdentityService(configuration);
 			services.AddBorrowersService(configuration);
 			services.AddBooksService(configuration);
 			return services;
@@ -24,7 +25,7 @@ namespace src.Services.Mono.DependencyInjection {
 
 		public static async Task<IApplicationBuilder> UseMonoService(this IApplicationBuilder app)
 		{
-			// await app.UseIdentityServices();
+			await app.UseIdentityServices();
 			await app.UseBorrowersService();
 
 			await app.UseBooksService();
